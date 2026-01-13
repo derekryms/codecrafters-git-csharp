59be4e9445b74b8e4fa512a2901b46ad7a45651c
Derek Ryms <derekryms@gmail.com>
2026-01-13 14:00:39 -0500

    Fourth stage

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 using System.IO.Compression;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 
 if (args.Length < 1)
 {
@@ -13,6 +14,7 @@ Console.Error.WriteLine("Logs from your program will appear here!");
 
 var command = args[0];
 var option = args.ElementAtOrDefault(1);
+var specifier = args.ElementAtOrDefault(2);
 
 switch (command)
 {
@@ -25,23 +27,14 @@ switch (command)
         break;
     case "cat-file":
     {
-        var hash = args.ElementAtOrDefault(2);
-
         switch (option)
         {
             case "-p":
-                if (hash is null)
+                if (specifier is null)
                     throw new ArgumentException($"No hash after {option} specified");
                 
-                var decompressedFileStream = new MemoryStream();
-                using (var compressedFileStream = File.Open($".git/objects/{hash[..2]}/{hash[2..]}", FileMode.Open, FileAccess.Read))
-                {
-                    using var decompressor = new ZLibStream(compressedFileStream, CompressionMode.Decompress);
-                    decompressor.CopyTo(decompressedFileStream);
-                }
-
-                var fullFileContents = Encoding.UTF8.GetString(decompressedFileStream.ToArray());
-                var userFileContents = fullFileContents.Split('\0')[1];
+                var fullFileContentBytes = GetGitObjectBytesFromHashHex(specifier);
+                var userFileContents = Encoding.UTF8.GetString(fullFileContentBytes).Split('\0')[1];
                 Console.Write(userFileContents);
                 break;
         }
@@ -76,6 +69,90 @@ switch (command)
         }
         break;
     }
+    case "ls-tree":
+    {
+        if (specifier is null)
+            throw new ArgumentException($"No hash after {option} specified");
+                
+        var fullFileContentBytes = GetGitObjectBytesFromHashHex(specifier);
+        var headerBytes = fullFileContentBytes.TakeWhile(b => b is not 0x00);
+        var header = Encoding.UTF8.GetString(headerBytes.ToArray()).Split('\u0020');
+        var type = header[0];
+        if (type is not "tree")
+            throw new ArgumentException($"Invalid hash type {type}");
+
+        List<GitTreeObject> treeObjects = [];
+        var size = int.Parse(header[1]);
+        var contentsWithoutHeader = fullFileContentBytes[^size..];
+
+        var lastNullByteIndex = -1;
+        var lastSpaceByteIndex = -1;
+        for (var i = 0; i < contentsWithoutHeader.Length; i++)
+        {
+            var currentByte = contentsWithoutHeader[i];
+
+            switch (currentByte)
+            {
+                case 0x20:
+                    lastSpaceByteIndex = i;
+                    break;
+                case 0x00:
+                {
+                    var modeStart = lastNullByteIndex == -1 ? 0 : lastNullByteIndex + 21;
+                    var modeBytes = contentsWithoutHeader[modeStart..lastSpaceByteIndex];
+                    var mode = int.Parse(Encoding.UTF8.GetString(modeBytes));
+                        
+                    var nameBytes = contentsWithoutHeader[++lastSpaceByteIndex..i];
+                    var name = Encoding.UTF8.GetString(nameBytes);
+                        
+                    lastNullByteIndex = i;
+                        
+                    var hashStart = i + 1;
+                    var hashEnd = hashStart + 20;
+                    var hash = contentsWithoutHeader[hashStart..hashEnd];
+                    var sha1Hash = SHA1.HashData(hash);
+                    treeObjects.Add(new GitTreeObject(sha1Hash, name, mode));
+                    break;
+                }
+            }
+        }
+                
+        var orderedTreeObjects = treeObjects.OrderBy(o => o.Name);
+        switch (option)
+        {
+            case "--name-only":
+                var sb = new StringBuilder();
+                foreach (var treeObject in orderedTreeObjects)
+                {
+                    sb.AppendLine(treeObject.Name);
+                }
+
+                Console.WriteLine(sb.ToString().TrimEnd());
+                break;
+        }
+        break;
+    }
     default:
         throw new ArgumentException($"Unknown command {command}");
+}
+
+return;
+
+byte[] GetGitObjectBytesFromHashHex(string hashHex)
+{
+    var decompressedFileStream = new MemoryStream();
+    using (var compressedFileStream = File.Open($".git/objects/{hashHex[..2]}/{hashHex[2..]}", FileMode.Open, FileAccess.Read))
+    {
+        using var decompressor = new ZLibStream(compressedFileStream, CompressionMode.Decompress);
+        decompressor.CopyTo(decompressedFileStream);
+    }
+
+    return decompressedFileStream.ToArray();
+}
+
+class GitTreeObject(byte[] sha1Hash, string name, int mode)
+{
+    public byte[] Sha1Hash { get; } = sha1Hash;
+    public string Name { get; } = name;
+    public int Mode { get; } = mode;
 }
